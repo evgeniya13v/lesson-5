95ab30e55d6330f1f6e219458bb8d2374089e1d4
Евгения <volkova13e@gmail.com>
2024-10-25 22:01:13 +0600

    практика

---
+++ main.js
@@ -0,0 +1,94 @@
+// let lenght; 
+// let width;
+// lenght = prompt("Введите длину комнаты (м):");
+// width = prompt ("Введите ширину комнаты (м):");
+// lenght = Number (lenght);
+// width = Number (width);
+// let square;
+// square = lenght * width; 
+// alert("Площадь комнаты равна " + square);
+
+// // let a = "hello";
+// // let b = "world";
+// // // alert(a + " " + b);
+// // alert(`${a} ${b}`);
+
+// let price;
+// let amount;
+// const month = 22;
+// price = +prompt ("стоимость одной поездки в общественном транспорте (тг):");
+// amount = +prompt ("количество поездок в день (шт):")
+// let pay;
+// pay = price * amount * month;
+// alert ("расходы на проезд за месяц: " + pay);
+
+
+// let tenge;
+// let exchange; 
+// let dollar;
+// tenge = prompt ("сумма в тенге");
+// exchange = prompt ("курс обмена тенге к доллару");
+// // tenge = Number (tenge);
+// // exchange = Number (exchange);
+// dollar = tenge / exchange;
+// alert ("сумму в долларах: " + dollar);
+
+
+// Индекс массы тела
+
+// let weight;
+// let height;
+//let index;
+//weight = +prompt ("Введите ваш вес (кг)");
+//height = +prompt ("Введите Ваш рост (см)");
+//index = weight / height ** 2;
+// alert ("индекс массы вашего дела: " + index);
+
+
+// Зарплата на руки. Примерный процент высчета с ЗП 21%. Это 0,21. 
+
+//let salary;
+//let money;
+//salary = +prompt ("Введите разамер Вашей заработной платы");
+//money = salary - (salary * 0.21);
+//alert ("зарплата на руки: " + money  );
+
+
+// В ресторане вариант 1. Когда считаем сразу с 0.15 - это 15% налогов
+
+// let check;
+//let tax;
+//let teas;
+//let total;
+//check =+prompt ("Введите сумму чека в тг");
+//teas =+prompt ("Введите сумму чаевых");
+//total = (check + (check * 0.15)) + teas;
+//total = check + (check * tax)+ teas;
+//alert (" итого заплатили в ресторане " + total)
+
+
+// В ресторане вариант 2. Когда вводим переменную посчитанную tax = 15/100;
+
+// let check;
+// let tax;
+// tax = 15/100;
+// let teas;
+// let total;
+// check =+prompt ("Введите сумму чека в тг");
+// teas =+prompt ("Введите сумму чаевых");
+// total = check + (check * tax)+ teas;
+// alert (" итого заплатили в ресторане " + total)
+
+// Поездка
+
+// let upset;
+// let consumption;
+// let petrol;
+// let total;
+// upset =+prompt ("Введите какое расстояние вы планируете проехать в км");
+// consumption =+prompt ("Введите какой у вас расход на 100 км в л ");
+// petrol =+prompt ("сколько стоит литр бензина тг");
+// total = upset / consumption * petrol;
+// alert (" стоиомсть бензина на поездку " + total)
+
+
